9df66eb5357e305477f3057428ee9b903dcf1cd5
smkqp8 <stephen.knutter@gmail.com>
2017-07-17 18:35:07 -0600

    finish widgets

---
+++ Layouts/app/src/main/java/com/stephenknutter/layouts/LayoutActivity.java
@@ -0,0 +1,13 @@
+package com.stephenknutter.layouts;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LayoutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.linear_experiement);
+    }
+}
