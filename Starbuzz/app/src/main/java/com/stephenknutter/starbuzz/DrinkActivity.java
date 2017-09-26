5d18fe2a1730014d7108908771ed6e9355009aa6
smkqp8 <stephen.knutter@gmail.com>
2017-09-25 22:06:12 -0600

    add

---
+++ Starbuzz/app/src/main/java/com/stephenknutter/starbuzz/DrinkActivity.java
@@ -0,0 +1,34 @@
+package com.stephenknutter.starbuzz;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class DrinkActivity extends Activity {
+
+    public static final String EXTRA_DRINKNO = "drinkNo";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_drink);
+
+        // Get id of drink clicked
+        int drinkNo = (Integer) getIntent().getExtras().get(EXTRA_DRINKNO);
+        Drink drink = Drink.drinks[drinkNo];
+
+        // Set image
+        ImageView photo = (ImageView) findViewById(R.id.photo);
+        photo.setImageResource(drink.getImageResourceId());
+        photo.setContentDescription(drink.getName());
+
+        // Set drink name
+        TextView name = (TextView) findViewById(R.id.name);
+        name.setText(drink.getName());
+
+        // Set drink description
+        TextView description = (TextView) findViewById(R.id.description);
+        description.setText(drink.getDescription());
+    }
+}
