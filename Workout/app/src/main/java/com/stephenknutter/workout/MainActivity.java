58134215a5615e1e9949c7fb89254a522980b73b
smkqp8 <stephen.knutter@gmail.com>
2017-09-30 20:33:03 -0600

    adding workouts

---
+++ Workout/app/src/main/java/com/stephenknutter/workout/MainActivity.java
@@ -0,0 +1,26 @@
+package com.stephenknutter.workout;
+
+import android.app.Activity;
+import android.app.FragmentTransaction;
+import android.os.Bundle;
+
+public class MainActivity extends Activity
+        implements WorkoutListFragment.WorkoutListListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void itemClicked(long id) {
+        WorkoutDetailFragment details = new WorkoutDetailFragment();
+        FragmentTransaction ft = getFragmentManager().beginTransaction();
+        details.setWorkout(id);
+        ft.replace(R.id.fragment_container, details);
+        ft.addToBackStack(null);
+        ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+        ft.commit();
+    }
+}
